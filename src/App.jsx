e2e98d9658b5bfe272129394c3f68aa9ce3f1865
Harsh srivastava <harshsrivastava2316@gmail.com>
2024-10-09 21:52:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main className='bg-black'>
-      navbar
-      hero
+     <Navbar />
+     <Hero />
+     <Highlights />
     </main>
   )
 }
