86b5c501ff85f48212c65bed96a2b01de9f0684b
Harsh srivastava <harshsrivastava2316@gmail.com>
2024-10-10 00:45:04 +0530

    commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -25,12 +25,12 @@ const Hero = () => {
     useGSAP(() => {
         gsap.to('#hero', {
             opacity: 1,
-            delay: 1.5,
+            delay: 2,
         })
         gsap.to('#cta', {
-            opacity:1,
-            y:-50,
-            delay:1.5
+            opacity: 1,
+            y: -50,
+            delay: 2,
         })
     }, [])
     return (
