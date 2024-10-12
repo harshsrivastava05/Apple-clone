3f2e28267e5d85b55c9c7999775b550d851a46d7
Harsh srivastava <harshsrivastava2316@gmail.com>
2024-10-12 22:28:57 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import Hero from './components/hero'
 import Highlights from './components/Highlights'
 import Navbar from './components/navbar'
+import Model from './components/model'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main className='bg-black'>
-     <Navbar />
-     <Hero />
-     <Highlights />
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
     </main>
   )
 }
